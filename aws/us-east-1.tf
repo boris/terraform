cb5a465e968260c03b787147ee7ae9f06cc1d612
Boris Quiroz <boris@insert-coin.org>
2018-03-07 22:34:11 -0300

    Add count and tags according to the count

--- aws/us-east-1.tf
+++ aws/us-east-1.tf
@@ -5,8 +5,14 @@ provider "aws" {
 }
 
 resource "aws_instance" "terraform-demo" {
+    count = 2
     ami = "ami-66506c1c"
     instance_type = "t2.micro"
     subnet_id = "subnet-3ecb2174"
-    vpc_security_group_ids = ["sg-3d13be4b"]
+    vpc_security_group_ids = [
+	"sg-3d13be4b"
+    ]
+    tags = {
+	Name = "terraform-demo-${count.index}"
+    }
 }
