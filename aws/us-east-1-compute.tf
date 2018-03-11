771690f5b8fb5a4f68d660d8cf65dda2ab03ea9a
Boris Quiroz <boris@insert-coin.org>
2018-03-11 01:54:11 -0300

    Remove demo instance

--- aws/us-east-1-compute.tf
+++ aws/us-east-1-compute.tf
@@ -4,19 +4,6 @@ provider "aws" {
     region  = "us-east-1"
 }
 
-resource "aws_instance" "terraform-demo" {
-    count = 0
-    ami = "ami-66506c1c"
-    instance_type = "t2.micro"
-    subnet_id = "subnet-3ecb2174"
-    vpc_security_group_ids = [
-	"sg-3d13be4b"
-    ]
-    tags = {
-	Name = "terraform-demo-${count.index}"
-    }
-}
-
 resource "aws_instance" "public-instance" {
     ami = "${var.ubuntu1604}"
     instance_type = "t2.micro"
