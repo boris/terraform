1160f7c749e070dd433c195a9ea39decb7c9c6fa
Boris Quiroz <boris@insert-coin.org>
2018-03-10 23:14:31 -0300

    AWS VPC and public subnet

---
+++ aws/us-east-1-networking.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "Staging" {
+    cidr_block = "10.240.0.0/16"
+    enable_dns_support = true
+    enable_dns_hostnames = true
+    tags {
+	"Name" = "Staging"
+    }
+}
+
+resource "aws_subnet" "staging-public" {
+    vpc_id = "${aws_vpc.Staging.id}"
+    cidr_block = "10.240.10.0/24"
+    availability_zone = "us-east-1a"
+    tags {
+	Name = "staging-public"
+    }
+}
