d19e39c3474ed86a2b3d237f9f9a522d29cb7ffb
Boris Quiroz <boris@insert-coin.org>
2018-03-11 01:11:58 -0300

    Add default route using IG

--- aws/us-east-1-networking.tf
+++ aws/us-east-1-networking.tf
@@ -16,7 +16,7 @@ resource "aws_subnet" "staging-public" {
     }
 }
 
-resource "aws_subnet" "stsaging-private" {
+resource "aws_subnet" "staging-private" {
     vpc_id = "${aws_vpc.Staging.id}"
     cidr_block = "10.240.20.0/24"
     availability_zone = "us-east-1b"
@@ -25,9 +25,17 @@ resource "aws_subnet" "stsaging-private" {
     }
 }
 
-resource "aws_internet_gateway" "staging-private-ig" {
+resource "aws_internet_gateway" "staging-ig" {
     vpc_id = "${aws_vpc.Staging.id}"
-    tags {
-        Name = "staging-private-ig"
+    tags = {
+        Name = "staging-ig"
     }
+}
+
+resource "aws_default_route_table" "staging-vpc" {
+    default_route_table_id = "${aws_vpc.Staging.default_route_table_id}"
+route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.staging-ig.id}"
+}
 }
