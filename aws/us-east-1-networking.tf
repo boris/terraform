9aae888933877c4ead3af30beb5a309df50b7c4e
Boris Quiroz <boris@insert-coin.org>
2018-03-10 23:19:20 -0300

    Fix var and add private subnet

--- aws/us-east-1-networking.tf
+++ aws/us-east-1-networking.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "Staging" {
     enable_dns_support = true
     enable_dns_hostnames = true
     tags {
-	"Name" = "Staging"
+	Name = "Staging"
     }
 }
 
@@ -13,5 +13,14 @@ resource "aws_subnet" "staging-public" {
     availability_zone = "us-east-1a"
     tags {
 	Name = "staging-public"
+    }
+}
+
+resource "aws_subnet" "stsaging-private" {
+    vpc_id = "${aws_vpc.Staging.id}"
+    cidr_block = "10.240.20.0/24"
+    availability_zone = "us-east-1b"
+    tags = {
+        Name = "staging-private"
     }
 }
