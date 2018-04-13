94a2cdd51142701a7eaf701bdcb4a9bd7f768381
Boris Quiroz <boris@insert-coin.org>
2018-04-13 16:12:38 -0300

    include region in the path

--- zsh/new-service.sh
+++ zsh/new-service.sh
@@ -4,12 +4,13 @@ set -e
 ask(){
     read -p 'Environment: ' environment
     read -p 'Service Name: ' service
+    read -p 'Region: ' region
 }
 
 main(){
     ask
-    mkdir ./$environment/$service
-    cp -a common/* $environment/$service
+    mkdir ./$environment/$region/$service
+    cp -a common/* $environment/$region/$service
 }
 
 main
