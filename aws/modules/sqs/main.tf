ff651adaac41e28390b332c72b0d50591074a40b
Boris Quiroz <boris@insert-coin.org>
2018-04-24 19:54:08 -0300

    add policy to the module

--- aws/modules/sqs/main.tf
+++ aws/modules/sqs/main.tf
@@ -1,3 +1,28 @@
 resource "aws_sqs_queue" "queue" {
     name = "${var.sqs_name}"
 }
+
+resource "aws_sqs_queue_policy" "policy" {
+    queue_url = "${aws_sqs_queue.queue.id}"
+    policy = "${data.template_file.policy_template.rendered}"
+}
+
+data "template_file" "policy_template" {
+    template = <<EOF
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "Sid1494364308343",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": ["${var.user_arn}"]
+      },
+      "Action": ["${join("\",\"", var.actions)}"],
+      "Resource": "${aws_sqs_queue.queue.arn}"
+    }
+  ]
+}
+EOF
+}
