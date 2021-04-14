e2c1c454846106132ea61c74923a5165e86bfbbb
Boris Quiroz <boris@insert-coin.org>
2021-04-14 07:43:38 -0400

    ping provider versions

--- k3s/main.tf
+++ k3s/main.tf
@@ -3,4 +3,11 @@ terraform {
     path = "./states/k3s/terraform.tfstate"
   }
   required_version = ">= 0.14.9"
+
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
 }
