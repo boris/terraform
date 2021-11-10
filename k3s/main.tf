dccb197d977dc5fafa92690774116774a14a2329
Boris Quiroz <boris@insert-coin.org>
2021-11-10 08:32:40 -0300

    Upgrade to terraform 1.0

--- k3s/main.tf
+++ k3s/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "local" {
     path = "./states/k3s/terraform.tfstate"
   }
-  required_version = ">= 0.14.9"
+  required_version = ">= 1.0.0"
 
   required_providers {
     kubernetes = {
