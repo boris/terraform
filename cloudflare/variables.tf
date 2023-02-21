954f0d03fd52cffc2d83438ffd847ef82ae81aff
Boris Quiroz <boris@insert-coin.org>
2023-02-21 16:20:22 -0300

    Minimal variables

---
+++ cloudflare/variables.tf
@@ -0,0 +1,13 @@
+variable "email" {
+  default     = null
+  description = "Email used to login on CF"
+}
+variable "api_key" {
+  default     = null
+  description = "Global API key"
+}
+
+variable "account_id" {
+  default     = null
+  description = "Account ID"
+}
