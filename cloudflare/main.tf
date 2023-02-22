4acd6db42b2ca0a764c441b525ef894edc7fc7c0
Boris Quiroz <boris@insert-coin.org>
2023-02-22 14:04:12 -0300

    Update provider
    Add tunnel config

--- cloudflare/main.tf
+++ cloudflare/main.tf
@@ -8,8 +8,26 @@ resource "random_password" "password" {
   special = true
 }
 
-resource "cloudflare_argo_tunnel" "home" {
+resource "cloudflare_tunnel" "home" {
   account_id = var.account_id
   name       = "home-tf"
   secret     = base64encode(random_password.password.result)
+}
+
+resource "cloudflare_tunnel_config" "config" {
+  account_id = var.account_id
+  tunnel_id  = cloudflare_tunnel.home.id
+
+  config {
+    origin_request {
+      no_tls_verify = false
+    }
+    ingress_rule {
+      hostname = "test.insert-coin.org"
+      service  = "https://localhost"
+    }
+    ingress_rule {
+      service = "http_status:404"
+    }
+  }
 }
